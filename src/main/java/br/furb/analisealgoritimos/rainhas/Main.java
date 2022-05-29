093cacc396206b572f6d23f4e325e99733c8acf0
Leeonardoo <leeonardo.deo@gmail.com>
2022-05-29 19:46:34 -0300

    Solução usando hill-climbing

--- src/main/java/br/furb/analisealgoritimos/rainhas/Main.java
+++ src/main/java/br/furb/analisealgoritimos/rainhas/Main.java
@@ -1,16 +1,157 @@
 package br.furb.analisealgoritimos.rainhas;
 
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Larson Kremer Vicente
+ * @author Leonardo de Oliveira
+ *
+ * Implementação da solução do problema n-rainhas usando hill climbing
+ * e reiniciando ao encontrar planícies.
+ */
 public class Main {
+    private static final Random random = new Random();
+
+    private static int n; //Número de rainhas
+    private static int numeroConflitos = 0; //Heuristica
+
+    //Informações sobre o resultado
+    private static int totalPassos = 0; //Total de passos
+    private static int totalReinicios = 0; //Número de reinícios (ao encontrar planícies)
 
     public static void main(String[] args) {
-        Rainha ultima;
-        ultima = null;
-        for (short i = 1; i <= 8; i++) {
-            ultima = new Rainha(i, ultima);
-            if (!ultima.achaSolucao()) {
-                throw new RuntimeException();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Digite o número de rainhas: ");
+            n = scanner.nextInt();
+
+            if (n != 2 && n != 3) {
+                break;
+            } else {
+                System.out.printf("Nenhuma solução para %d rainhas.\n", n);
             }
         }
-        ultima.imprime();
+
+        Rainha[] estadoInicial = novoTabuleiro();
+        Rainha[] tabuleiroSolucao = buscaSolucao(estadoInicial);
+
+        //Solução
+        System.out.println();
+        System.out.println("Cenário inicial:");
+        mostrarTabuleiro(estadoInicial);
+
+        System.out.println();
+        System.out.println("Solução:");
+        mostrarTabuleiro(tabuleiroSolucao);
+
+        System.out.println();
+        System.out.printf("Número de reinícios: %d\n", totalReinicios);
+        System.out.printf("Total de passos: %d\n", totalPassos);
+    }
+
+    public static Rainha[] novoTabuleiro() {
+        Rainha[] tabuleiro = new Rainha[n];
+
+        for (int i = 0; i < n; i++) {
+            tabuleiro[i] = new Rainha(random.nextInt(n), i);
+        }
+
+        return tabuleiro;
+    }
+
+    //1 = rainha; 0 = vazio
+    private static void mostrarTabuleiro(Rainha[] tabuleiro) {
+        int[][] posicoes = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            posicoes[tabuleiro[i].getLinha()][tabuleiro[i].getColuna()] = 1;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(posicoes[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-".repeat((n * 4) - 1));
+        }
+    }
+
+    public static int getConflitos(Rainha[] tabuleiro) {
+        int conflitos = 0;
+
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = i + 1; j < tabuleiro.length; j++) {
+                if (tabuleiro[i].temConflito(tabuleiro[j])) {
+                    conflitos++;
+                }
+            }
+        }
+
+        return conflitos;
+    }
+
+    public static Rainha[] minimizarConflitos(Rainha[] tabuleiroAtual) {
+        Rainha[] proximoTabuleiro = new Rainha[n];
+        Rainha[] auxTabuleiro = new Rainha[n];
+
+        for (int i = 0; i < n; i++) {
+            Rainha rainha = new Rainha(tabuleiroAtual[i].getLinha(), tabuleiroAtual[i].getColuna());
+            proximoTabuleiro[i] = rainha;
+            auxTabuleiro[i] = rainha;
+        }
+
+        int conflitosAtuais = getConflitos(tabuleiroAtual);
+        int menorConflitos = conflitosAtuais;
+        int auxConflitos;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                auxTabuleiro[i - 1] = new Rainha(tabuleiroAtual[i - 1].getLinha(), tabuleiroAtual[i - 1].getColuna());
+            }
+            auxTabuleiro[i] = new Rainha(0, auxTabuleiro[i].getColuna());
+            for (int j = 0; j < n; j++) {
+                auxConflitos = getConflitos(auxTabuleiro);
+
+                if (auxConflitos < menorConflitos) {
+                    menorConflitos = auxConflitos;
+                    for (int k = 0; k < n; k++) {
+                        proximoTabuleiro[k] = new Rainha(auxTabuleiro[k].getLinha(), auxTabuleiro[k].getColuna());
+                    }
+                }
+
+                if (auxTabuleiro[i].getLinha() != n - 1) {
+                    auxTabuleiro[i].moverLinha();
+                }
+            }
+        }
+
+        if (menorConflitos == conflitosAtuais) {
+            proximoTabuleiro = novoTabuleiro();
+            totalReinicios++;
+            numeroConflitos = getConflitos(proximoTabuleiro);
+        } else {
+            numeroConflitos = menorConflitos;
+        }
+
+        totalPassos++;
+        return proximoTabuleiro;
+    }
+
+    public static Rainha[] buscaSolucao(Rainha[] tabuleiroInicial) {
+        Rainha[] tabuleiroAtual = tabuleiroInicial;
+        int conflitosAtuais;
+        conflitosAtuais = getConflitos(tabuleiroAtual);
+
+        //Reinicia até que nenhuma rainha se ataque
+        while (conflitosAtuais != 0) {
+            tabuleiroAtual = minimizarConflitos(tabuleiroAtual);
+            conflitosAtuais = numeroConflitos;
+        }
+
+        return tabuleiroAtual;
     }
 }
--- src/main/java/br/furb/analisealgoritimos/rainhas/Rainha.java
+++ src/main/java/br/furb/analisealgoritimos/rainhas/Rainha.java
@@ -1,60 +1,35 @@
 package br.furb.analisealgoritimos.rainhas;
 
 /**
- * Queen.java
- * basedao no codigo de Timothy Budd
+ * @author Larson Kremer Vicente
+ * @author Leonardo de Oliveira
  */
 public class Rainha {
     private int linha;
     private final int coluna;
-    private final Rainha vizinha;
 
-    public Rainha(int coluna, Rainha vizinha) {
+    public Rainha(int linha, int coluna) {
+        this.linha = linha;
         this.coluna = coluna;
-        linha = 1;
-        this.vizinha = vizinha;
     }
 
-    public boolean achaSolucao() {
-        while (vizinha != null && vizinha.temConflito(linha, coluna))
-            if (!avanca())
-                return false;
-        return true;
+    public int getLinha() {
+        return linha;
     }
 
-    public boolean avanca() {
-        if (linha < 8) {
-            linha++;
-            return achaSolucao();
-        }
-        if (vizinha != null) {
-            if (!vizinha.avanca())
-                return false;
-            if (!vizinha.achaSolucao())
-                return false;
-        } else
-            return false;
-        linha = 1;
-        return achaSolucao();
+    public int getColuna() {
+        return coluna;
     }
 
-    private boolean temConflito(int tlin, int tcol) {
-        int colDif = tcol - coluna;
-        if ((linha == tlin) || (linha + colDif == tlin) ||
-                (linha - colDif == tlin))
+    public void moverLinha() {
+        linha++;
+    }
+
+    public boolean temConflito(Rainha r) {
+        if (coluna == r.getColuna() || linha == r.getLinha()) {
             return true;
-        if (vizinha != null)
-            return vizinha.temConflito(tlin, tcol);
-        return false;
-    }
-
-    public String toString() {
-        return ("[" + coluna + ", " + linha + "]");
-    }
-
-    public void imprime() {
-        if (vizinha != null)
-            vizinha.imprime();
-        System.out.println(this);
+        } else {
+            return Math.abs(coluna - r.getColuna()) == Math.abs(linha - r.getLinha());
+        }
     }
 }
